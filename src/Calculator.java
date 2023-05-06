88ab86ecd6843cc059965fed7697fd54bfe4610e
GlebEm <gleb.emelyanov.2013@mail.ru>
2023-05-06 12:04:32 +0300

    CalcJava

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
         String input = scanner.nextLine();
         String[] str = input.split(" ");
         if (str[0].length()>2||str[2].length()>2) {
-            throw new Exception("Не удовлетворяет условию (вводятся только целые числа)");
+            throw new Exception("Не удовлетворяет условию (вводятся только целые числа от 1 до 10)");
         }
         int a = Integer.parseInt(str[0]);
         String c = str[1];
@@ -17,7 +17,7 @@ public class Calculator {
         if (str.length<3) {
             throw new Exception("Строка не является математической операцией");
         }
-        if (a < 0 || a > 10 || b < 0 || b > 10) {
+        if (a <= 0 || a > 10 || b <= 0 || b > 10) {
             throw new Exception("Введённое значение не удовлетворяет условию (a и b - целые числа от 1 до 10)");
         }
         switch (operator) {
