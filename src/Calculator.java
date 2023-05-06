8c9bcc0e35fed96c376a92494fe51ea35ab7bed8
GlebEm <gleb.emelyanov.2013@mail.ru>
2023-05-06 12:36:18 +0300

    CalcJava

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         String[] str = input.split(" ");
-        if (str[0].length()>2||str[2].length()>2) {
+        if (str.length <= 2) {
+            throw new Exception("Строка не является математической операцией или пропущен пробел между оператором и операндом");
+        }
+        if (str[0].length() > 2 || str[2].length() > 2) {
             throw new Exception("Не удовлетворяет условию (вводятся только целые числа от 1 до 10)");
         }
         int a = Integer.parseInt(str[0]);
         String c = str[1];
         char operator = c.charAt(0);
         int b = Integer.parseInt(str[2]);
-        if (str.length>3) {
+        if (str.length > 3) {
             throw new Exception("Формат математической операции не удовлетворяет заданию - два операнда и один оператор(+, -, /, *)");
-        }
-        if (str.length<3) {
-            throw new Exception("Строка не является математической операцией");
         }
         if (a <= 0 || a > 10 || b <= 0 || b > 10) {
             throw new Exception("Введённое значение не удовлетворяет условию (a и b - целые числа от 1 до 10)");
@@ -35,6 +36,6 @@ public class Calculator {
                 break;
             default:
                 throw new Exception("Символ не является математической операцией");
-            }
         }
     }
+}
